a2b475d10eaa043aa51a345d0f0152949988706c
guka nozadze <gukanozadze@gmail.com>
2019-10-17 02:53:26 +0400

    added mongodb basic setup to playground

---
+++ playground/mongodb.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+
+
+const connectionUrl = 'mongodb://localhost:27017'
+const dbName = 'names-db'
+
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+
+  const db = client.db(dbName)
+
+  db.collection('users').insertOne({
+    name: 'Saba'
+  }).then((result) => {
+    console.log(result.ops) 
+  }).catch((err) => {
+    console.log(err)
+  })
+
+
+})
